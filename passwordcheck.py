b87184b458b53534a39d3a37fec7b8d51646e58c
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-11-24 21:55:04 -0500

    tested code against main. the arguments work and the response yields from main.

--- passwordcheck.py
+++ passwordcheck.py
@@ -1,6 +1,7 @@
 import requests
 # the most common library used in any code involving apis.
 import hashlib
+import sys
 
 # url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
 
@@ -17,9 +18,29 @@ def request_api_data(query_char):
     if res.status_code != 200:
         raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again!')
     return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
 def pwned_api_check(password):
     # check password if it exists in API response
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
 
-request_api_data('41810')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times...you should change your password!')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+    return 'done!'
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
